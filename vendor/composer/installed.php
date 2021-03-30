ea33c2e41c8b5ed3ed57c6ddcf40abf4d0d8a6bf
randy <randy.semedo@hotmail.fr>
2021-03-30 10:23:08 +0200

    zdz

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'b65070f27a62adf730341936cd16efdc2cb71598',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'b65070f27a62adf730341936cd16efdc2cb71598',
+    ),
+  ),
+);
